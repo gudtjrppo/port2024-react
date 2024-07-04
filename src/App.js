e2dec59c594c400540aaca488b01793b9c673f62
June Lee <gudtjrppo@gmail.com>
2024-07-04 17:16:14 +0900

    컴퍼넌트 완료

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import HomeView from './views/HomeView';
 
 const App = () => {
-    return <div> Appfff </div>;
+    return(
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" elment={<HomeView />} />
+            </Routes>
+        </BrowserRouter>
+    )
 };
 
 
